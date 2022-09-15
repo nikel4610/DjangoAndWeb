11fe556d71834efee168290a3e804f81d762b403
Hyun-woo <nikel4610@gmail.com>
2022-09-15 16:33:46 +0900

    bookmark

--- firstproject/mysite/mysite/urls.py
+++ firstproject/mysite/mysite/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bookmark.views import BookmarkLV, BookmarkDV
+
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookmark/', BookmarkLV.as_view(), name='index'),
+    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+
+    # path('bookmark/', ListView.as_view(model=Bookmark), name='index'),
+    # path('bookmark/<int:pk>/', DetailView.as_view(model=Bookmark), name='detail'),
 ]
