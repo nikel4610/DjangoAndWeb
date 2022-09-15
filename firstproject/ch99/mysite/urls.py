a7220784ab22da851a1e1e9ad3342dfcef3b6c9b
Hyun-woo <nikel4610@gmail.com>
2022-09-15 15:32:59 +0900

    reset

--- firstproject/ch99/mysite/urls.py
+++ firstproject/ch99/mysite/urls.py
@@ -1,4 +1,4 @@
-"""firstproject URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('firstApp.urls')),
+    path('', include('bookmark.urls')),
 ]
